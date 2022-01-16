29a9f35f9b5e0d24a836c922b0dba4fe719d45bb
Gyan <gyan.satapathy@gmail.com>
2022-01-16 11:15:00 +0530

    Added Pre existing user

--- src/main/java/com/gs/product/domain/ApplicationUser.java
+++ src/main/java/com/gs/product/domain/ApplicationUser.java
@@ -6,9 +6,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
+@Table(name = "APPLICATIONUSER")
 public class ApplicationUser {
     @Id
     @GeneratedValue(generator = "uuid2")
